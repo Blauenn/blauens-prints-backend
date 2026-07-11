6fc25645a68e4be798ac723ea6b1ca92d7e25947
Blauen <85344725+Blauenn@users.noreply.github.com>
2026-07-12 00:49:54 +0700

    Barebones file selector

--- src/routes/image/create.tsx
+++ src/routes/image/create.tsx
@@ -19,35 +19,51 @@ function RouteComponent() {
         <h1 className="text-lg font-bold text-blue-500 mb-4">« Return</h1>
       </Link>
 
-      <div className="grid grid-cols-7 gap-4 h-[80vh]">
+      <input
+        ref={inputRef}
+        type="file"
+        multiple
+        accept="image/*"
+        className="hidden"
+        onChange={(e) => {
+          if (!e.target.files) return;
+
+          const imported = importFiles(Array.from(e.target.files));
+
+          setItems((prev) => [...prev, ...imported]);
+
+          if (selectedIndex === null && imported.length > 0) {
+            setSelectedIndex(0);
+          }
+
+          e.target.value = "";
+        }}
+      />
+
+      <div className="grid grid-cols-7 gap-4 h-[85vh]">
         {/* Dropzone */}
-        <div className="col-span-2 w-full h-full p-3 bg-gray-200 rounded-xl">
-          {items.length < 1 ? (
-            <div className="flex flex-col items-center justify-center gap-3 w-full h-full">
+        <div
+          onDragOver={(e) => {
+            e.preventDefault();
+          }}
+          onDrop={(e) => {
+            e.preventDefault();
+
+            const imported = importFiles(Array.from(e.dataTransfer.files));
+
+            setItems((prev) => [...prev, ...imported]);
+
+            if (selectedIndex === null && imported.length > 0) {
+              setSelectedIndex(0);
+            }
+          }}
+          className="col-span-2 w-full h-full bg-gray-200 rounded-xl"
+        >
+          {items.length === 0 ? (
+            <div className="flex flex-col items-center justify-center gap-3 w-full h-full px-4">
               <h1 className="text-xs text-center opacity-50">
                 Drag and drop files to get started...
               </h1>
-
-              <input
-                ref={inputRef}
-                type="file"
-                multiple
-                accept="image/*"
-                className="hidden"
-                onChange={(e) => {
-                  if (!e.target.files) return;
-
-                  const imported = importFiles(Array.from(e.target.files));
-
-                  setItems((prev) => [...prev, ...imported]);
-
-                  if (selectedIndex === null && imported.length > 0) {
-                    setSelectedIndex(0);
-                  }
-
-                  e.target.value = "";
-                }}
-              />
 
               <button
                 onClick={() => inputRef.current?.click()}
@@ -57,7 +73,39 @@ function RouteComponent() {
               </button>
             </div>
           ) : (
-            <div className="flex flex-col gap-1"></div>
+            <div className="w-full">
+              <div className="flex flex-row justify-between items-center gap-2 px-4 py-3 bg-gray-300 rounded-lg mb-2 w-full">
+                <h1 className="text-xs">
+                  {items.length > 1 ? `${items.length} files` : "1 file"}
+                </h1>
+                <div className="flex flex-row gap-2">
+                  <button className="flex items-center justify-center text-white bg-red-400 opacity-50 hover:opacity-100 rounded-md w-[20px] h-[20px]">
+                    -
+                  </button>
+                  <button
+                    onClick={() => inputRef.current?.click()}
+                    className="flex items-center justify-center text-white bg-green-400 opacity-50 hover:opacity-100 rounded-md w-[20px] h-[20px]"
+                  >
+                    +
+                  </button>
+                </div>
+              </div>
+              <div className="px-3 py-1 flex flex-col gap-1">
+                {items.map((item, index) => (
+                  <button
+                    key={`${item.file.name}-${index}`}
+                    onClick={() => setSelectedIndex(index)}
+                    className={`rounded-lg p-2 text-[10px] text-left truncate ${
+                      selectedIndex === index
+                        ? "bg-gray-300"
+                        : "hover:bg-gray-300"
+                    }`}
+                  >
+                    {item.file.name}
+                  </button>
+                ))}
+              </div>
+            </div>
           )}
         </div>
 
